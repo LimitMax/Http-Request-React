54222a55d052acfe5142d9f6171f6dd1c3644e63
LimitMax <Taufiqqurohman98@gmail.com>
2022-04-17 20:30:34 +0700

    Http Request

---
+++ src/App.js
@@ -0,0 +1,174 @@
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  // List Card
+  constructor(props) {
+    super(props);
+    this.state = {
+      members: [],
+      first_name: "",
+      last_name: "",
+      buttonDisabled: false,
+      formStatus: "create",
+      memberIdSelected: null,
+    };
+  }
+
+  //
+  componentDidMount() {
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then((response) => {
+        this.setState({ members: response.data.data });
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+  //
+  inputOnchangeHandler = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  //   Submit
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    this.setState({ buttonDisabled: true });
+    var payload = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+    };
+    var url = "";
+    if (this.state.formStatus === "create") {
+      url = "https://reqres.in/api/users";
+      this.addMember(url, payload);
+    } else {
+      url = `https://reqres.in/api/users/${this.state.memberIdSelected}`;
+      this.editMember(url, payload);
+    }
+  };
+
+  //add
+  addMember = (url, payload) => {
+    axios
+      .post(url, payload)
+      .then((response) => {
+        console.log(response);
+        var members = [...this.state.members];
+        members.push(response.data);
+        this.setState({ members, buttonDisabled: false, first_name: "", last_name: "" });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    alert("Data berhasil disimpan!!");
+  };
+
+  //edit member
+  editMember = (url, payload) => {
+    axios
+      .put(url, payload)
+      .then((response) => {
+        var members = [...this.state.members];
+        var indexMember = members.findIndex((member) => member.id === this.state.memberIdSelected);
+
+        members[indexMember].first_name = response.data.first_name;
+        members[indexMember].last_name = response.data.last_name;
+
+        this.setState({
+          members,
+          buttonDisabled: false,
+          first_name: "",
+          last_name: "",
+          formStatus: "create",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  //delete
+  deleteButtonHandler = (id) => {
+    var url = `https://reqres.in/api/users/${id}`;
+    axios
+      .delete(url)
+      .then((response) => {
+        if (response.status === 204) {
+          var members = [...this.state.members];
+          var index = members.findIndex((member) => member.id === id);
+          members.splice(index, 1);
+          this.setState({ members });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    alert("Data berhasil dihapus!!");
+  };
+
+  //Edit Button
+  editButtonHandler = (member) => {
+    this.setState({
+      first_name: member.first_name,
+      last_name: member.last_name,
+      formStatus: "edit",
+      memberIdSelected: member.id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container ">
+        <h1>Latihan HTTP Request DevSchool</h1>
+        <div className="row bg-info">
+          <div className="col-md-6" style={{ border: "2px solid green" }}>
+            <h2>Members</h2>
+            <div className="row">
+              {this.state.members.map((member, index) => (
+                <div className="col-md-6" key={index}>
+                  <div className="card" style={{ margin: 10 }}>
+                    <div className="card-body">
+                      <h5 className="card-title">No: {member.id}</h5>
+                      <h5 className="card-title">{member.first_name}</h5>
+                      <h5 className="card-title">{member.last_name}</h5>
+                      <button className="btn btn-success m-1" onClick={() => this.editButtonHandler(member)}>
+                        Edit
+                      </button>
+                      <button className="btn btn-danger m-1" onClick={() => this.deleteButtonHandler(member.id)}>
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-md-6" style={{ border: "2px solid green" }}>
+            <h2>Form {this.state.formStatus}</h2>
+            <form onSubmit={this.onSubmitHandler}>
+              <div className="form-gruop mb-3">
+                <label className="form-label">First Name</label>
+                <input type="text" className="form-control" name="first_name" value={this.state.first_name} onChange={this.inputOnchangeHandler} />
+              </div>
+              <div className="form-gruop mb-3">
+                <label className="form-label">First Name</label>
+                <input type="text" className="form-control" name="last_name" value={this.state.last_name} onChange={this.inputOnchangeHandler} />
+              </div>
+
+              <button type="submit" className="btn btn-primary m-1" disabled={this.state.buttonDisabled}>
+                Simpan
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
